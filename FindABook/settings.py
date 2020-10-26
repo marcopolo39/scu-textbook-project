ada9e26c5352fe0b4e171b04a67dc7b2ed53549f
Marco Maldonado <marco@Marcos-MacBook-Pro.local>
2020-10-26 12:41:41 -0700

    added django textbook app

--- FindABook/settings.py
+++ FindABook/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #packages
+
+    #custom
+    'textbook',
 ]
 
 MIDDLEWARE = [
