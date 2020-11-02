c5278951e9976cb98dafc7e09a6961a13403466b
Marco <39marcom@gmail.com>
2020-11-02 15:49:29 -0800

    init

--- FindABook/urls.py
+++ FindABook/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("frontend.urls")),
     path('admin/', admin.site.urls),
+    path("api/account", include("accounts.urls"))
 ]
