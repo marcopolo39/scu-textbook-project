befdd709163dcf63fefcc46ea473b7ed9873387f
Marco <39marcom@gmail.com>
2020-11-05 21:45:42 -0800

    CRUD & Login endpoints

--- FindABook/urls.py
+++ FindABook/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("frontend.urls")),
     path('admin/', admin.site.urls),
-    path("api/account", include("accounts.urls"))
+    path("api/account/", include("accounts.urls"))
 ]
---
+++ accounts/serializers.py
@@ -0,0 +1,43 @@
+from abc import ABC
+
+from django.contrib.auth import authenticate
+from .models import User
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'school', 'location')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'school', 'location', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['first_name'],
+            validated_data['last_name'],
+            validated_data['school'],
+            validated_data['location'],
+            validated_data['password'],
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
