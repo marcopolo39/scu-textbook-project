2365a452d0e9db7448caf18b56298b4436bfca4c
Peter Allgeier <peter.allgeier@nextworld.net>
2020-11-09 12:58:40 -0700

    serializer

--- textbook/models.py
+++ textbook/models.py
@@ -1,18 +1,14 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 class Textbook(models.Model):
-    # Textbook_id should be the combination of the user_id and isbn number followed by a count
-    # The count is necessary if they are selling the same textbook more than once
+    textbookId = models.CharField(max_length=100, unique=True, primary_key=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     isbn = models.CharField(max_length=13)
     price = models.DecimalField(max_digits=9,decimal_places=2)
     state = models.IntegerField()
-    textbookPicture = models.CharField(max_length=100, default="temp")
-    textbookId = models.CharField(max_length=26, default="temp", unique=True)
-
-    #REQUIRED_FIELDS =
+    #textbookPicture = models.CharField(max_length=100)
     
     def __str__(self):
         return self.textbookId
-
-    # Should filter using Textbook.objects.filter(textbookId__startswith='User_id')
