06b98c24b2baabd6befbcf10707fd5aa90c4ac93
Marco <39marcom@gmail.com>
2020-11-10 22:14:42 -0800

    renamed message model to messages model to avoid conflicts with django messages

--- messaging/models.py
+++ messaging/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth import get_user_model
 
 
 # Create your models here.
-class Message(models.Model):
+class Messages(models.Model):
     sender = models.ForeignKey(get_user_model(), related_name='sender', on_delete=models.CASCADE)
     receiver = models.ForeignKey(get_user_model(), related_name='recipient', on_delete=models.CASCADE)
     message = models.CharField(max_length=1500)
     timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.sender.username + " -> " + self.receiver.username
