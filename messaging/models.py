02fb8778d3d0d1824b9b7f2362e399009c9a1e51
Marco <39marcom@gmail.com>
2020-11-10 21:50:39 -0800

    messaging model

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(get_user_model(), related_name='sender', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(get_user_model(), related_name='recipient', on_delete=models.CASCADE)
+    message = models.CharField(max_length=1500)
+    timestamp = models.DateTimeField(auto_now=True)
