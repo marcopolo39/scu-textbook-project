4f65facdf5e678bb3fe5d34a22337b6c3d7609e1
liamconnole <LConnole@scu.edu>
2020-12-04 11:57:31 -0500

    nav bar revision

--- frontend/src/actions/messageActions.js
+++
@@ -1,8 +0,0 @@
-import { SET_RECEIVER } from "./actions";
-
-export const setReceiver = (receiver) => {
-  return {
-    type: SET_RECEIVER,
-    payload: receiver,
-  };
-};
--- frontend/src/hooks/useLoader.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-
-export const useLoader = () => {
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {}, [loaded]);
-
-  return [loaded, setLoaded];
-};
